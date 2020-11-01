9ed2aea290de09d1c4ba8f00b24e2dd7d716741c
zejiang-unsw <47626771+zejiang-unsw@users.noreply.github.com>
2020-11-01 14:19:46 +1100

    update ssm
    - add par and wt
    - only show not converge message

--- R/WaveletSS.R
+++ R/WaveletSS.R
@@ -72,12 +72,13 @@ WaveletSS<- function(y,wf="haar",J,boundary,NForecast,...)
 #'
 #' @examples
 #' 
-WaveletSS.xreg<- function(y,xreg,NForecast, order=1, freq=12, method=F,flag.comb="BG",...)
+WaveletSS.xreg<- function(y,xreg,NForecast, order=1, freq=4, par, wt, method=F,flag.comb="BG",...)
 {
   n1 <- length(y)-NForecast
   train_o <- y[1:n1]
   test_o <- y[-c(1:n1)]
-  
+  #stopifnot(frequency(y)==freq) 
+  stopifnot(length(y)==nrow(as.matrix(xreg)))
   ndim <- ncol(xreg)
   
   AllWaveletForecast <- NULL;AllWaveletPrediction <- NULL
@@ -86,9 +87,11 @@ WaveletSS.xreg<- function(y,xreg,NForecast, order=1, freq=12, method=F,flag.comb
     # Fitting of SS model to predictor individually             #
     #-----------------------------------------------------------#
     ssm <- function(parm, X) {
-      dlmModPoly(order, dV=parm[1]) + dlmModSeas(freq) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3],0))
+      #dlmModPoly(order, dV=parm[1]) + dlmModSeas(freq) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3],0))
+      dlmModPoly(order, dV=parm[1]) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3],0))
     }
-    par=rep(0,3)
+    #par=rep(0,3)
+    
     for(i in 1:ndim){
       
       WaveletFit <- dlmMLE(y=as.ts(train_o), parm=par, X=xreg[1:n1,i], build=ssm) 
@@ -96,7 +99,8 @@ WaveletSS.xreg<- function(y,xreg,NForecast, order=1, freq=12, method=F,flag.comb
       
       #fit.xreg <- auto.arima(xreg[1:n1,i]) # work with both seasonal and non-seasonal data
       #modFit <- ssm(parm=WaveletFit$par, X=forecast(fit.xreg,h=NForecast)$mean)
-      modFit <- ssm(parm=WaveletFit$par, X=xreg[1:n1,i])
+      #modFit <- ssm(parm=WaveletFit$par, X=xreg[1:n1,i])
+      modFit <- ssm(parm=WaveletFit$par, X=xreg[,i])
       
       WaveletFilter <- dlmFilter(c(train_o, rep(NA, NForecast)), modFit)
       WaveletPredict <- WaveletFilter$f[1:(n1)]
@@ -127,21 +131,26 @@ WaveletSS.xreg<- function(y,xreg,NForecast, order=1, freq=12, method=F,flag.comb
     # Fitting of SS model to predictor together                 #
     #-----------------------------------------------------------#
     ssm <- function(parm, X) {
-      dlmModPoly(order, dV=parm[1]) + dlmModSeas(freq) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3], rep(0,ncol(X))))
+      #dlmModPoly(order, dV=parm[1]) + dlmModSeas(freq) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3], rep(0,ncol(X))))
+      #dlmModPoly(order, dV=parm[1]) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3], rep(1,ncol(X))))
+      dlmModPoly(order, dV=parm[1]) + dlmModSeas(freq) + dlmModReg(X=X, dV=parm[2], dW=c(parm[3], wt))
+      #dlmModPoly(order, dV=parm[1]) + dlmModReg(X=X, dV=parm[2], dW=parm[3:(3+ncol(X))])
     }
-    par=rep(0,3)
+    #par=rep(0,3)
     
-    WaveletFit <- dlmMLE(y=as.ts(train_o), parm=par, X=xreg[1:n1,], build=ssm) 
-    if(WaveletFit$convergence==0) print("converge") else print("Did not converge")
+    WaveletFit <- dlmMLE(y=train_o, parm=par, X=xreg[1:n1,], build=ssm) 
+    #if(WaveletFit$convergence==0) print("converge") else print("Did not converge")
+    if(WaveletFit$convergence!=0) print("Did not converge")
     
-    modFit <- ssm(parm=WaveletFit$par, X=xreg[1:n1,])
+    modFit <- ssm(parm=WaveletFit$par, X=xreg)
+    #modFit <- ssm(parm=WaveletFit$par, X=xreg[1:n1,])
     
     WaveletFilter <- dlmFilter(c(train_o, rep(NA, NForecast)), modFit)
     WaveletPredict <- WaveletFilter$f[1:(n1)]
     WaveletForecast <- WaveletFilter$f[-c(1:(n1))]    
     
-    Accuracy.Train = accuracy(as.ts(WaveletPredict), train_o)
-    Accuracy.Test =  accuracy(as.ts(WaveletForecast), test_o)
+    Accuracy.Train = accuracy(WaveletPredict, train_o)
+    Accuracy.Test =  accuracy(WaveletForecast, test_o)
     FinalForecast = WaveletForecast
     FinalPrediction=WaveletPredict
   }
